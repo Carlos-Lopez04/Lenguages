6867445d20428122c7f43797ae178ce9dddde8d7
Carlos-Lopez04 <clopez8@uabc.edu.mx>
2025-10-08 12:28:43 -0600

    new topics

--- Dart/print.dart
+++ Dart/print.dart
@@ -1,14 +1,14 @@
-//* Forma corta (recomendada para ejemplos simples)
-
-void main() {
-  // tu código aquí
-}
-
-//* Forma con arrow function
-void main() => runApp();
-
-//* Forma con parámetros (menos común)
-void main(List<String> arguments) {
-  // tu código aquí
-}
-
+/**
+ //? Forma corta (recomendada para ejemplos simples)
+ * void main() {
+ // tu código aquí
+ * }
+ * 
+ //? Forma con arrow function
+ * void main() => runApp();
+ * 
+ //? Forma con parámetros (menos común)
+ * void main(List<String> arguments) 
+ // tu código aquí
+ * }
+*/
--- Dart/variables.dart
+++ Dart/variables.dart
@@ -1,6 +1,22 @@
 /** Tipos de variables
  * 
  * int -> números enteros
+ * double -> numeros decimales
+ * num -> Se puede utilizar para otros
+ * 
+ * string -> cadenas
+ * $ -> variables simples
+ * {3+5} -> expresiones
+ * 
+ * bool -> boolean
+ * 
+ * list -> coleccion ordenada
+ * 
+ * map -> coleccion de clave-valor
+ * 
+ * dynamic -> Desactiva la verificacion de tipos
+ * 
+ * var -> Infiere el tipo automaticamente
  */
 
 //* Declaracion de variables con var (Inferencia de tipo)
@@ -8,13 +24,16 @@
 void main() {
 
 var nombre = "Juan";
-var edad = 19;
+var edad;
 var precio = 20.50;
 var estatus = true;
 
+edad=20;
+
 print('Declaracion de variables con var (Inferencia de tipo)\n');
 print(nombre);
-print("\nSu nombre es $nombre");
+print('''\nSu nombre es $nombre
+y su edad es $edad años''');
 print('========================');
 
 //* Declaracion de variables con tipo explicito
@@ -28,7 +47,7 @@ print('Declaracion de variables con tipo explicito\n');
 print('''Nombre: $nombres
 Edad: $edades''');
 
-print('\nElla vende un chocolate a $precios');
+print('\nElla vende un chocolate a $precios pesos');
 print('========================');
 
 //* Declaracon con tipo dinamico
@@ -44,11 +63,11 @@ variableDinamica = true;     // ✅ Ahora es bool
 print(variableDinamica);
 print('========================');
 
-//* Declaración con final (Inmutable - valor conocido en tiempo de ejecución
+//* Declaración con final (Inmutable - valor conocido en tiempo de ejecución)
 final nombrefin = "Carlos";
 final int edadfin = 40;
 // nombre = "Pedro"; ❌ ERROR - No se puede reasignar (Inmutable - valor conocido en tiempo de ejecución)
 
 print('Declaracion con final\n');
-print('Su nombre es $nombrefin y su edad es $edadfin\n');
+print('Su nombre es $nombrefin y su edad es $edadfin año\n');
 }
